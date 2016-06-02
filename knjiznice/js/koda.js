b62fe1fa2f9253e102c23f6905468a73e0c87a7f
Ziga Babnik <zb1996@student.uni-lj.si>
2016-06-02 16:53:59 +0000

    Digging myself a deepdeepdeepdeep grave :/

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -145,6 +145,9 @@ var partyDataohgod;
 
 function preberiMeritveVitalnihZnakov() {
 	
+	//$("#rezultatMeritveVitalnihZnakovBasic").empyt();
+	//$("#rezultatMeritveVitalnihZnakov").empty();
+	
 	var sessionId = getSessionId();
 	var ehrId = $("#meritveVitalnihZnakovEHRid").val();
 	var results;
@@ -163,8 +166,58 @@ function preberiMeritveVitalnihZnakov() {
 	    	success: function (data) {
 				var party = data.party;
 				partyDataohgod = data.party;
-				$("#rezultatMeritveVitalnihZnakovBasic").append("<div class='row'><div class='col-lg-8 col-md-8 col-sm-8'><h4>Ime: "+ party.firstNames +" Priimek: "+ party.lastNames+" ehrId :"+ ehrId +"<h4></div> "+
-        		"</div><button type='button' class='btn btn-primary btn-xs' onclick='izpisiTezoVisino()'>+</button>");
+				//$("#rezultatMeritveVitalnihZnakovBasic").html("<div class='row'><div class='col-lg-8 col-md-8 col-sm-8'><h4>BMI uporabnika "+ party.firstNames +" "+ party.lastNames+" je </h4>");
+        			$("#rezultatMeritveVitalnihZnakov").empty();
+					results = "<div class='row>'<div class='col-lg-10 col-md-10 col-sm-10'>";
+					$.ajax({
+					    url: baseUrl + "/view/" + ehrId + "/" + "weight",
+					    type: 'GET',
+					    headers: {"Ehr-Session": sessionId},
+					    success: function (res) {
+					    	if (res.length > 0) {
+						    	results += "  <b>Telesna teža:</b> ";
+                    			teza = res[0].weight;
+                    			teza_global = res[0].weight;
+						        results += res[0].weight + " " +res[0].unit;
+					    	} else {
+					    		$("#preberiMeritveVitalnihZnakovSporocilo").html(
+                    "<span class='obvestilo label label-warning fade-in'>" +
+                    "Ni podatkov!</span>");
+					    	}
+					    },
+					    error: function() {
+					    	$("#preberiMeritveVitalnihZnakovSporocilo").html(
+                  "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+                  JSON.parse(err.responseText).userMessage + "'!");
+					    }
+					});
+					$.ajax({
+					    url: baseUrl + "/view/" + ehrId + "/" + "height",
+					    type: 'GET',
+					    headers: {"Ehr-Session": sessionId},
+					    success: function (res) {
+					    	if (res.length > 0) {
+					    		visina = res[0].height;
+					    		visina_global = res[0].height;
+					    		results += "   <b>Telesna višina:</b> ";
+						        results += res[0].height +" "+res[0].unit;
+								results += "</div></div>";
+								$("#rezultatMeritveVitalnihZnakovBasic").append(("<div class='row'><div class='col-lg-8 col-md-8 col-sm-8'><h4>BMI uporabnika "+ party.firstNames +" "+ party.lastNames+" je "+
+									"<b>"+ teza/Math.pow((visina/100), 2)) +"</b></h4></div> " +
+        		    			"</div><button type='button' class='btn btn-primary btn-xs' onclick='izpisiTezoVisino()'>+</button></div>");
+						        //$("#rezultatMeritveVitalnihZnakov").append(results);
+					    	} else {
+					    		$("#preberiMeritveVitalnihZnakovSporocilo").html(
+                    "<span class='obvestilo label label-warning fade-in'>" +
+                    "Ni podatkov!</span>");
+					    	}
+					    },
+					    error: function() {
+					    	$("#preberiMeritveVitalnihZnakovSporocilo").html(
+                  "<span class='obvestilo label label-danger fade-in'>Napaka '" +
+                  JSON.parse(err.responseText).userMessage + "'!");
+					    }
+					});
 			}
 		});
 	}
@@ -215,7 +268,7 @@ function izpisiTezoVisino() {
 					    		visina_global = res[0].height;
 					    		results += "   <b>Telesna višina:</b> ";
 						        results += res[0].height +" "+res[0].unit;
-						        results += "   <b>BMI:</b> "+ (teza/Math.pow((visina/100), 2));
+						        //results += "   <b>BMI:</b> "+ (teza/Math.pow((visina/100), 2));
 								results += "</div></div>";
 						        $("#rezultatMeritveVitalnihZnakov").append(results);
 					    	} else {
@@ -235,46 +288,109 @@ function izpisiTezoVisino() {
 
 function makeGraph() {
 
-	var margin = {top: 20, right: 20, bottom: 30, left: 50}, width = 960 - margin.left - margin.right, height = 500 - margin.top - margin.bottom;
+	var w = 500;
+	var h = 500;
+	
+	var data = [{"x":10, "y":10},
+				{"x":20, "y":20},
+				{"x":30, "y":30},
+				{"x":30, "y":30},
+				{"x":40, "y":40},
+				{"x":50, "y":50},
+				{"x":60, "y":60},]
 
-	var svg = d3.select('#rezultatMeritveVitalnihZnakov')
-				.append('svg')
+	var svg = d3.select('#preberiMeritveVitalnihZnakovSporocilo').append('svg')
 				.attr({
-						"width" : width + margin.right + margin.left, 
-						"height": height + margin.top + margin.bottom
+						"width" : w, 
+						"height": h
 					})
-					.append('g')
-						.attr("transform", "translate(" + margin.left + ',' + margin.right + ")");
-
-	var x = d3.scale.linear().range([0, width]);
-	var y = d3.scale.linear().range([height, 0]);
 
 	var xAxis = d3.svg.axis().scale(x).orient("bottom");
 	var yAxis = d3.svg.axis().scale(y).orient("left");
 	
 	var line = d3.svg.line()
-						.x(function(d) { return x(d.teza)})
-						.y(function(d) { return y(d.visina)});
+						.x(function(d) {  return d.x})
+						.y(function(d) {  return h-d.y});
 						
-	//Tommorow
-	
-	d3.cvs("bmi.csv", function(napaka, data){
+	var path = svg.append("path")
+					.attr({
+						d:line(data),
+						"fill":"none",
+						"stroke":"green"
+					})
+					
+	//var label = svg.selectAll("text")
+	//				.data()
+						
+	/*
+	d3.tsv("bmi.tsv", type, function(napaka, data){
 		if(napaka) console.log("ERR");
-		data.forEach(function(d) {
-			d.weight = +d.weight;
-			d.height = +d.height;
-		})
+		console.log(data);
 		
-		xScale.domain(data.map(function(d) {
-			return d.height;
+		x.domain(d3.extent(data, function(d) {
+			console.log(d.teza);
+			return d.teza;
 		}))
 		
-		yScale.domain(data.map(function(d) {
-			return d.weight;
+		y.domain(d3.extent(data, function(d) {
+			console.log(d.visina);
+			return d.visina;
 		}))
+		
+		svg.append("g")
+					.attr("class", "x axis")
+					.attr("transform", "translate(0," + height + ")")
+					.call(xAxis)
+
+		
+		svg.append("g")
+					.attr("class", "y axis")
+					.attr("transform", "rotate(-90)")
+					.call(yAxis)
+					
+		svg.append("path")
+    		.attr("class", "line")
+    		.attr("d", line);
 	})
 	
+	function type(d) {
+		  d.teza = +d.teza;
+		  d.visina = +d.visina;
+		  return d;
+	}
+	*/
 }
 
+
+function initMap() {
+  var chicago = {lat: 41.85, lng: -87.65};
+  var indianapolis = {lat: 39.79, lng: -86.14};
+
+  var map = new google.maps.Map($('#map'), {
+    center: chicago,
+    scrollwheel: false,
+    zoom: 7
+  });
+
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  // Set destination, origin and travel mode.
+  var request = {
+    destination: indianapolis,
+    origin: chicago,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+
+  // Pass the directions request to the directions service.
+  var directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      // Display the route on the map.
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
